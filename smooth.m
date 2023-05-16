d4f72d7c9b137d8a4514dd2cd9f082938538aa46
DamlaH <56763977+DamlaH@users.noreply.github.com>
2023-05-16 20:49:58 +0200

    Add files via upload

---
+++ smooth.m
@@ -0,0 +1,9 @@
+% List of open inputs
+nrun = X; % enter the number of runs here
+jobfile = {'D:\FU MCNB\Neurocognitive Methods and Data Analysis\Second Semester\Data\Week 2\smooth_job.m'};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(0, nrun);
+for crun = 1:nrun
+end
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
