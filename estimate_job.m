923311b308d324e4e0a2b39b9ef902d3fa416f2c
DamlaH <56763977+DamlaH@users.noreply.github.com>
2023-06-07 15:22:31 +0200

    Add files via upload

---
+++ estimate_job.m
@@ -0,0 +1,8 @@
+%-----------------------------------------------------------------------
+% Job saved on 07-Jun-2023 13:50:18 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.fmri_est.spmmat = {'D:\FU MCNB\Neurocognitive Methods and Data Analysis\Second Semester\Data\Week 2\classical\SPM.mat'};
+matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
